d4c2fa1e89f62e849f0c3e5ff7f1fa4f3f730a4d
Parker Corbitt <parkercorbitt36@gmail.com>
2023-12-11 13:03:33 -0500

    fixed image rendering

--- Chunk-Hub/src/main/java/net/parkerasa/chunkhub/menu/viewerScreen.java
+++ Chunk-Hub/src/main/java/net/parkerasa/chunkhub/menu/viewerScreen.java
@@ -6,8 +6,11 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 
 import javax.imageio.ImageIO;
+import javax.management.DynamicMBean;
 
 import com.mojang.blaze3d.platform.NativeImage;
+
+import io.netty.channel.epoll.Native;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
@@ -24,6 +27,8 @@ public class viewerScreen extends Screen {
     private int[] imageWidths;
     private int[] imageHeights;
     private File[] listOfFiles;
+    private final File folder = new File(System.getProperty("user.home") + File.separator
+            + "AppData\\Roaming\\.minecraft\\screenshots");
 
     public viewerScreen(Component title) {
         super(title);
@@ -46,13 +51,17 @@ public class viewerScreen extends Screen {
         };
 
         Button.OnPress nextPress = (button) -> {
-            if (index < 30) {
+            if (index < listOfFiles.length - 1) {
                 index++;
             }
         };
 
         Button.OnPress backPress = (button) -> {
             this.minecraft.setScreen(new photoScreen(Component.literal("test")));
+        };
+
+        Button.OnPress loadPress = (button) -> {
+            load_file();
         };
 
         Button backButton = new Button.Builder(Component.literal("Back"), backPress)
@@ -70,12 +79,16 @@ public class viewerScreen extends Screen {
                 .size(buttonWidth, buttonHeight)
                 .build();
 
+        Button loadButton = new Button.Builder(Component.literal("Load"), loadPress)
+                .pos(buttonXMiddle, this.height - 60)
+                .size(buttonWidth, buttonHeight)
+                .build();
+
         this.addRenderableWidget(backButton);
         this.addRenderableWidget(prevButton);
         this.addRenderableWidget(nextButton);
+        this.addRenderableWidget(loadButton);
 
-        File folder = new File(System.getProperty("user.home") + File.separator
-                + "AppData\\Roaming\\.minecraft\\screenshots");
         listOfFiles = folder.listFiles();
 
         if (images == null) {
@@ -103,21 +116,37 @@ public class viewerScreen extends Screen {
 
         super.render(graphics, mouseX, mouseY, partialTick);
 
-        if (images[index] == null) {
-            load_file();
+        if (listOfFiles.length != 0) {
+
+            if(images[index] == null) {
+                load_file();
+            }
+
+            TextureManager textureManager = Minecraft.getInstance().getTextureManager();
+
+            try {
+                textureManager.bindForSetup(images[index]);
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Error: Not Bind" + e);
+            }
+
+            textureManager.getTexture(images[index]).setFilter(true, true);
+
+            int width = imageWidths[index];
+            int height = imageHeights[index];
+
+            int x = this.width / 2 - width / 2;
+            int y = this.height / 2 - height / 2;
+
+            try {
+                graphics.blit(images[index], x, y, 0, 0, width, height);
+                System.out.println("Blit");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Error: Not Blit" + e);
+            }
         }
-
-        TextureManager textureManager = Minecraft.getInstance().getTextureManager();
-        textureManager.bindForSetup(images[index]);
-        textureManager.getTexture(images[index]).setFilter(false, false);
-
-        int width = imageWidths[index];
-        int height = imageHeights[index];
-
-        int x = this.width / 2 - width / 2;
-        int y = this.height / 2 - height / 2;
-
-        graphics.blit(images[index], y, mouseX, mouseY, partialTick, width, height, x, y);
 
     }
 
@@ -127,12 +156,22 @@ public class viewerScreen extends Screen {
     }
 
     public void load_file() {
+
+        if (listOfFiles.length == 0) {
+            return;
+        }
+
+        System.out.println("Load File");
+        System.out.println(listOfFiles[index].getName());
+
         BufferedImage image = null;
+        File location = new File(folder + File.separator + listOfFiles[index].getName());
+
         try {
-            image = ImageIO.read(new File(System.getProperty("user.home") + File.separator
-                    + "AppData\\Roaming\\.minecraft\\screenshots\\" + listOfFiles[index].getName()));
+            image = ImageIO.read(location);
         } catch (IOException e) {
             e.printStackTrace();
+            System.out.println("Error: Not Read" + e);
         }
 
         int width = image.getWidth();
@@ -141,41 +180,48 @@ public class viewerScreen extends Screen {
         imageWidths[index] = width;
         imageHeights[index] = height;
 
-        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * width * height);
-        int[] pixels = new int[width * height];
-        image.getRGB(0, 0, width, height, pixels, 0, width);
+        int[] pixels = image.getRGB(0, 0, width, height, null, 0, width);
 
         for (int i = 0; i < pixels.length; i++) {
             int argb = pixels[i];
             pixels[i] = (argb & 0xFF00FF00) | ((argb & 0xFF0000) >> 16) | ((argb & 0xFF) << 16);
         }
 
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * width * height);
+
         for (int y = height - 1; y >= 0; y--) {
             for (int x = 0; x < width; x++) {
-
                 int pixel = pixels[y * width + x];
-
                 byteBuffer.put((byte) ((pixel >> 16) & 0xFF)); // Red component
                 byteBuffer.put((byte) ((pixel >> 8) & 0xFF)); // Green component
                 byteBuffer.put((byte) (pixel & 0xFF)); // Blue component
-                byteBuffer.put((byte) ((pixel >> 24) & 0xFF)); // Alpha component. Only for
-                // RGBA
+                byteBuffer.put((byte) ((pixel >> 24) & 0xFF)); // Alpha component. Only for RGBA
             }
         }
 
-        byteBuffer.flip(); //FOR THE LOVE OF GOD DO NOT FORGET THIS
+        byteBuffer.flip();
 
-        NativeImage nativeImage = new NativeImage(NativeImage.Format.RGBA, width, height, false);
-        
         try {
-            nativeImage.read(byteBuffer);
+
+            // Create a new NativeImage
+            NativeImage blammoimage = new NativeImage(NativeImage.Format.RGBA, width, height, false);
+
+            // Set the pixels of the NativeImage
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    int pixel = pixels[y * width + x];
+                    blammoimage.setPixelRGBA(x, y, pixel);
+                }
+            }
+
+            DynamicTexture texture = new DynamicTexture(blammoimage);
+            ResourceLocation resourceLocation = Minecraft.getInstance().getTextureManager()
+                    .register("blammoimage", texture);
+            images[index] = resourceLocation;
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        DynamicTexture texture = new DynamicTexture(nativeImage);
-        images[index] = Minecraft.getInstance().getTextureManager().register("chunkhub", texture);
-
     }
 
 }
